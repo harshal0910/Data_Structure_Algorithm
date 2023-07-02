17e6e40b6014bbb6870e82e6df3acd2613ec185b
harshal0910 <130588038+harshal0910@users.noreply.github.com>
2023-07-02 22:40:12 +0530

    Create Add_Two_Numbers.java

---
+++ Add_Two_Numbers.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode(0);
+        ListNode temp=dummy;
+        int carry=0;
+        while(l1!=null||l2!=null){
+            int x=(l1!=null)?l1.val:0;
+            int y=(l2!=null)?l2.val:0;
+            int sum=carry+x+y;
+            carry=sum/10;
+            temp.next=new ListNode(sum%10);
+            temp=temp.next;
+            if(l1!=null)
+                l1=l1.next;
+            if(l2!=null)
+                l2=l2.next;
+        }
+        if(carry>0)
+            temp.next=new ListNode(carry);
+        return dummy.next;
+    }
+    
+}
