46d605c0c4cd966ea21b9379026fc125685d0a77
Harshal Rajendra Nagtilak <harshal0910@Harshals-MacBook-Air.local>
2025-06-10 01:02:38 +0530

    Sub Array Sum

---
+++ Arrays/subarraySum.java
@@ -0,0 +1,32 @@
+// You are given an integer array nums of size n. For each index i where 0 <= i < n, define a subarray nums[start ... i] where start = max(0, i - nums[i]).
+
+// Return the total sum of all elements from the subarray defined for each index in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,3,1]
+
+// Output: 11
+
+
+class Solution {
+    public int subarraySum(int[] nums) {
+
+        int reqAns = 0;
+
+        for(int i=0;i<nums.length;i++){
+            int start = Math.max(0,i-nums[i]);
+            int ans = 0;
+            while(start<=i){
+                ans += nums[start];
+                start++;
+            }
+            reqAns += ans;
+        }
+
+        return reqAns;
+        
+    }
+}
