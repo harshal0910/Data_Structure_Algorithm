9054db2614f7158524f346312da5478dc24a1fef
harshal0910 <harshalnagtilak2809@gmail.com>
2025-06-12 01:42:41 +0530

    sumOddLengthSubarrays optimized solution

--- Arrays/sum_odd_length_subarrays.java
+++ Arrays/sum_odd_length_subarrays.java
@@ -39,3 +39,22 @@ class Solution {
         return ans;
     }
 }
+
+
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int n = arr.length;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int left = i + 1;
+            int right = n - i;
+
+            int totalSubarrays = left * right;
+            int oddCount = (totalSubarrays + 1) / 2;
+
+            ans += arr[i] * oddCount;
+        }
+        return ans;
+    }
+}
