11d069d37add7bf6a8c5cfeead769872732fbccf
harshal0910 <harshalnagtilak2809@gmail.com>
2025-06-12 00:50:06 +0530

    sumOddLengthSubarrays

---
+++ Arrays/sum_odd_length_subarrays.java
@@ -0,0 +1,41 @@
+// Given an array of positive integers arr, return the sum of all possible odd-length subarrays of arr.
+
+// A subarray is a contiguous subsequence of the array.
+
+ 
+
+// Example 1:
+
+// Input: arr = [1,4,2,5,3]
+// Output: 58
+// Explanation: The odd-length subarrays of arr and their sums are:
+// [1] = 1
+// [4] = 4
+// [2] = 2
+// [5] = 5
+// [3] = 3
+// [1,4,2] = 7
+// [4,2,5] = 11
+// [2,5,3] = 10
+// [1,4,2,5,3] = 15
+// If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+
+
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int n = arr.length;
+        int[] prefix = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + arr[i];
+        }
+
+        int ans = 0;
+        for (int start = 0; start < n; start++) {
+            for (int length = 1; start + length <= n; length += 2) {
+                int end = start + length;
+                ans += prefix[end] - prefix[start];
+            }
+        }
+        return ans;
+    }
+}
