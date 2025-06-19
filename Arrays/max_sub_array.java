a550889ba6d41479688371424e99f30fd0a7dc09
harshal0910 <harshalnagtilak2809@gmail.com>
2025-06-19 09:59:26 +0530

    LCQ Maximum Subarray

--- Arrays/max_sub_array.java
+++ Arrays/max_sub_array.java
@@ -24,32 +24,55 @@
 // 1 <= nums.length <= 105
 // -104 <= nums[i] <= 104
 
+
+//Using Prefix Sum
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+
+//         int[] prefixSum = new int[nums.length];
+//         prefixSum[0] = nums[0];
+//         for(int i=1;i<nums.length;i++){
+//             prefixSum[i] = prefixSum[i-1] + nums[i];
+//         }
+//         int ans = Integer.MIN_VALUE;
+//         for(int i=0;i<nums.length;i++){
+            
+//             for(int j=i;j<nums.length;j++){
+//                 int sum;
+//                 if(i==0){
+//                     sum = prefixSum[j];
+//                 }
+//                 else{
+//                     sum = prefixSum[j] - prefixSum[i-1]; 
+//                 }
+//                 ans = Math.max(ans,sum);
+                 
+//             }
+
+            
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int maxSubArray(int[] nums) {
 
-        int[] prefixSum = new int[nums.length];
-        prefixSum[0] = nums[0];
-        for(int i=1;i<nums.length;i++){
-            prefixSum[i] = prefixSum[i-1] + nums[i];
-        }
-        int ans = Integer.MIN_VALUE;
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
         for(int i=0;i<nums.length;i++){
-            
-            for(int j=i;j<nums.length;j++){
-                int sum;
-                if(i==0){
-                    sum = prefixSum[j];
-                }
-                else{
-                    sum = prefixSum[j] - prefixSum[i-1]; 
-                }
-                ans = Math.max(ans,sum);
-                 
+            currSum += nums[i];
+
+            if(currSum>maxSum){
+                maxSum = currSum;
             }
 
-            
+            if(currSum<0){
+                currSum=0;
+            }
         }
-
-        return ans;
+        return maxSum;
     }
 }
